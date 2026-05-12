db947af300be57a8a7326ea91014aad2b43e64e0
mahipatil13 <mahiiipatil13@gmail.com>
2026-05-12 20:23:30 +0530

    Updated code

---
+++ python1.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 / num2
+
+print("\nResults")
+print("Addition =", sum)
+print("Subtraction =", difference)
+print("Multiplication =", product)
+print("Division =", division)
